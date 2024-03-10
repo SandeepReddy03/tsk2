136d2155cae3c9c26e0e18c39a93ba6b7cffeab0
SandeepReddy03 <modugusandeep2@gmail.com>
2024-03-10 09:56:02 +0530

    tasktwo

---
+++ App.js
@@ -0,0 +1,20 @@
+
+
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ProductList from "./ProductList.js";
+import ProductDetails from "./ProductDetails.js";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<ProductList/>} />
+        <Route path="/product/:id" element={<ProductDetails/>} />
+      </Routes>
+    </Router>
+  );
+};
+export default App;
